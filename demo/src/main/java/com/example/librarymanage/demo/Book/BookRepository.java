7cd7d7c6056a8b02fb3b5870b902ce6b46d54dd7
Nguyen Phi Hung <dev.phihung@gmail.com>
2023-05-24 20:31:17 +0700

    delete 2 folder

--- demo/src/main/java/com/example/librarymanage/demo/Book/BookRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.librarymanage.demo.Book;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<BookItem, Long> {
-}
